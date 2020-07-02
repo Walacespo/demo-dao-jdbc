a4b64e5f45b68698f7fbf6602178e231a3a026c2
Walace da Silva <walacespo@gmail.com>
2020-07-02 12:02:18 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,9 +8,10 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
 		Department dp = new Department(1,"Book");
 		
-		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
 		
 		System.out.println(seller);
 	
